2059c71b7c9a7af8361f9291198b1da4c5b41652
Annie Kramer <anniekramer@dropbox.com>
2014-07-21 19:43:25 -0700

    Integrate functions

--- dropbook_analysis_decomposed.py
+++ dropbook_analysis_decomposed.py
@@ -28,30 +28,32 @@ def parse_field_names(arbitrary_list):
         for key in arbitrary_dict.keys():
             field_names.add(key)
     return field_names
-    # print "The field names in " + " are as follows: " + ", ".join(str(x) for x in field_names) + "."
 
 def parse_field_items(field_names, arbitrary_list):
     '''
     Given the field names in a dictionary nested in an arbitrary list,
     print the field items (values for respective keys in dictionaries).
+    Flatten lists and convert all field items to lowercase. 
     '''
     # must get all of the values for a given field name
     for field_name in field_names:
         field_content = [dictionary[field_name] for dictionary in arbitrary_list]
-        all_field_items = []
         field_content_list = flatten(field_content)
+        # field_content_lowercase = [x.lower() for x in field_content_list]
+        category_field_items = []
         for x in field_content_list:
             if x is not None:
-                all_field_items.append(x)
-    return all_field_items
+                category_field_items.append(x)
+    return category_field_items
 
 def print_field_content(field_names, arbitrary_list):
     '''
     Print out all field names followed by colon and all field items for that category.
     '''
+    # pass in parse_field_items function to make this more concise
     for field_name in field_names:
-        field_content = [dictionary[field_name] for dictionary in arbitrary_list]
-        print field_name
+        category_field_items = parse_field_items(field_names, arbitrary_list)
+        print field_name + ":" + ", ".join(str(x) for x in category_field_items) + "."
 
 if __name__ == '__main__':
     main()
@@ -60,6 +62,7 @@ if __name__ == '__main__':
 '''
 TODO
 
+apply counter for each field name
 for each field name, write to csv file for that field
 
 ideal CSV format:
