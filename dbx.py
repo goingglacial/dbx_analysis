2fb756bc6eba6290998bcf83809437398380513b
Annie Kramer <anniekramer@dropbox.com>
2014-07-24 15:49:53 -0700

    Fix printing for field_names and field_items

--- dbx.py
+++ dbx.py
@@ -15,50 +15,45 @@ def main():
     field_names = parse_field_names(arbitrary_list)
 
     parse_field_names(arbitrary_list)
-    print_field_item_count(field_names, arbitrary_list)
+    parse_field_items(field_names, arbitrary_list)
 
 def parse_field_names(arbitrary_list):
     '''
     Given a list of dictionaries nested in a list, returns
-    the keys from each of the dictionaries.
+    the keys from each of the dictionaries, where keys are
+    field names.
     '''
     field_names = set()
     for arbitrary_dict in arbitrary_list:
         for key in arbitrary_dict.keys():
             field_names.add(key)
     return field_names
-    # print " ,".join(str(x) for x in field_names)
 
-def print_field_item_count(field_names, arbitrary_list):
+def parse_field_items(field_names, arbitrary_list):
+    '''
+    Given a field name (key for a person dictionary), return
+    the field items (values) for that particular field name.
+    '''
+    # field_names = parse_field_names(arbitrary_list)
     for field_name in field_names:
-        all_field_items = compute_field_item_count(field_name, arbitrary_list)
-        print field_name + ":" + " ,".join(str(x) for x in all_field_items)
+        field_content = [p[field_name] for p in arbitrary_list]
+        category_field_items = []
+        field_items_list = flatten(field_content)
+
+        for x in field_items_list:
+            if x is not None:
+                category_field_items.append(x)
+        return category_field_items
 
 def compute_field_item_count(field_name, arbitrary_list):
     '''
-    Given the name of a field in a particular list, computes
-    top ten most frequently occurring items for that field.
-    '''
-    # get all of the values for a given field_name
-    field_content = [p[field_name] for p in arbitrary_list]
-
-    all_field_items = []
-    field_content_list = flatten(field_content)
-    for x in field_content_list:
-        if x is not None:
-            all_field_items.append(x)
-
-    return all_field_items
+    Given the name of a field in a particular list, counts the 
+    number of times that field items occur within that category.
+    '''    
     # get count for items
     # field_items_counted = Counter(all_field_items)
     # return field_items_counted
 
-
-
-'''
-        field_item_count = compute_field_item_count(field_name, arbitrary_list)
-    print field_name + ":" + " ,".join(str(x) for x in field_item_count)
-'''
 
 if __name__ == '__main__':
     main()
