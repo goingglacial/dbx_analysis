d4b80d335a933967f357bf4942a2a91b218583d7
Annie Kramer <anniekramer@dropbox.com>
2014-07-24 22:35:33 -0700

    Write counter to CSV files

--- dbx.py
+++ dbx.py
@@ -2,6 +2,7 @@
 from collections import Counter
 from compiler.ast import flatten
 
+import csv
 import itertools
 import json
 import sys
@@ -15,7 +16,9 @@ def main():
     field_names = parse_field_names(arbitrary_list)
     for field_name in field_names:
         category_field_items = parse_field_items(field_name, arbitrary_list)
+        field_items_ranked = compute_field_item_count(field_name, category_field_items, arbitrary_list)
         compute_field_item_count(field_name, category_field_items, arbitrary_list)
+        write_to_csv(field_name, field_items_ranked, arbitrary_list)
 
 def parse_field_names(arbitrary_list):
     '''
@@ -49,19 +52,21 @@ def parse_field_items(field_name, arbitrary_list):
 
     return category_field_items
 
-# function that returns individual list for field_name 
-
 def compute_field_item_count(field_name, category_field_items, arbitrary_list):
     '''
     Per field name, compute occurrences of field items within
     that category.
     '''   
     field_items_ranked = Counter(category_field_items)
-    print field_name.upper() +":"
-    for x in field_items_ranked:
-        print x, field_items_ranked[x]
+    return field_items_ranked
+    # print field_name.upper() +":"
+    # for x in field_items_ranked:
+    #     print x, field_items_ranked[x]
 
-# def write_to_csv(field_name, category_field_items, arbitrary_list):
+def write_to_csv(field_name, field_items_ranked, arbitrary_list):
+    writer = csv.writer(open('dbx.csv', 'wb'))
+    for key, value in field_items_ranked.items():
+        writer.writerow([key, value])
 
 if __name__ == '__main__':
     main()
